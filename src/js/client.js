d731253d2919e17cd30214513c9e40bf1ce46d60
Keerthi harish <keerthi.harish10@gmail.com>
2017-04-16 05:15:29 +0530

    Fixed build issues

--- src/js/client.js
+++ src/js/client.js
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { Provider } from "react-redux"
 
-import Layout from "./components/Layout"
+import Layout from "./layouts/Layout"
 import store from "./store"
 
 const app = document.getElementById('app')
