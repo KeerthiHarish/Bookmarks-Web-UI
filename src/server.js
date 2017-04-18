7251debbfa4dab8033fce52cef969df3dee3aafc
Keerthi harish <keerthi.harish10@gmail.com>
2017-04-18 18:30:07 +0530

    Add route for bundle

--- src/server.js
+++ src/server.js
@@ -5,10 +5,12 @@ module.exports = {
   app: function () {
     const app = express()
     const indexPath = path.join(__dirname, './index.html')
+    const bundlePath = path.join(__dirname, './bundle.js')
     const publicPath = express.static(path.join(__dirname, './'))
 
     app.use('./', publicPath)
     app.get('/', function (_, res) { res.sendFile(indexPath) })
+    app.get('/bundle.js', function (_, res) { res.sendFile(bundlePath) })
 
     return app
   }
